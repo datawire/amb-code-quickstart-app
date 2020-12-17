093421f430207b325b3ed9ab4126a0681b4730f7
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2020-12-17 11:07:20 +0000

    Update node app variables with better data

--- FrontEndNodeService/app.js
+++ FrontEndNodeService/app.js
@@ -1,9 +1,11 @@
 var argv = require('minimist')(process.argv.slice(2));
 var express = require("express");
 
-const location = "cloud"
+const environment = "cloud"
+const recordCount = 99999999999
 
 console.log(argv);
+
 var color = String(argv.c);
 
 var app = express();
@@ -12,12 +14,12 @@ app.get("/color", (req, res, next) => {
   res.json(color);
  });
 
-app.get("/location", (req, res, next) => {
-  res.json(location);
+app.get("/environment", (req, res, next) => {
+  res.json(environment);
  });
 
-app.get("/data", (req, res, next) => {
-  res.json("Lots of data");
+app.get("/recordCount", (req, res, next) => {
+  res.json(recordCount);
  });
 
 app.listen(3000, () => {
