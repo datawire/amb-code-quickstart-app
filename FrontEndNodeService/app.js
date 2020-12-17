e6bad1d52be4a769f965ae256b7c9fd370125d78
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2020-12-17 09:05:34 +0000

    Basic Java website and node service

--- FrontEndNodeService/app.js
+++ FrontEndNodeService/app.js
@@ -1,7 +1,25 @@
-var http = require('http');
+var argv = require('minimist')(process.argv.slice(2));
+var express = require("express");
 
+const location = "cloud"
 
-http.createServer(function (req, res) {
-  res.write('Hello World!'); 
-  res.end(); 
-}).listen(8080);
+console.log(argv);
+var color = String(argv.c);
+
+var app = express();
+
+app.get("/color", (req, res, next) => {
+  res.json(color);
+ });
+
+app.get("/location", (req, res, next) => {
+  res.json(location);
+ });
+
+app.get("/data", (req, res, next) => {
+  res.json("Lots of data");
+ });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
