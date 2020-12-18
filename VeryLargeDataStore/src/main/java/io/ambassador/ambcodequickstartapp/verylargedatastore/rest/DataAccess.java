0c7d14c6c5e8d8acf67dd281067af4f18ea23781
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2020-12-18 09:09:56 +0000

    Add data store docker config

--- VeryLargeDataStore/src/main/java/io/ambassador/ambcodequickstartapp/verylargedatastore/rest/DataAccess.java
+++ VeryLargeDataStore/src/main/java/io/ambassador/ambcodequickstartapp/verylargedatastore/rest/DataAccess.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataAccess {
-    @GetMapping("/")
+    @GetMapping("/recordCount")
     public String getRecordCount() {
         return "99999999999999";
     }
